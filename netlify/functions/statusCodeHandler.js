f59121164ed6d18c8164b8ef68958316f97e31a3
Dave Judd <dave_judd@starkey.com>
2023-02-28 13:36:27 -0600

    Match all codes

---
+++ netlify/functions/statusCodeHandler.js
@@ -0,0 +1,21 @@
+exports.handler = async function (event, context) {
+
+    const code = event.path.match(/([^\/]*)\/*$/)[1];
+
+    return {
+      statusCode: code,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(
+        [
+          {
+            message: "You got the code!"
+          }
+        ]
+      ),
+    };
+  }
