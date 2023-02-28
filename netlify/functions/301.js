118b5ed12c5442b1cbb25c76a849aaf31a5248d6
Dave Judd <dave_judd@starkey.com>
2023-02-28 13:00:05 -0600

    Add 301

---
+++ netlify/functions/301.js
@@ -0,0 +1,18 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 301,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(
+      [
+        {
+          message: "You got the code!"
+        }
+      ]
+    ),
+  };
+}
